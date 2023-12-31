907c2845ec415bdf531a662461fba83158af6c56
Benzazoua <billalben252@gmail.com>
2023-12-31 21:11:30 +0100

    Hero Section Structure & Style & Js

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,3 +43,58 @@ window.addEventListener("scroll", function () {
     hideHeader();
   } else header.classList.remove("active");
 });
+
+// ? === Hero Slider
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+const heroSliderNextBtn = document.querySelector("[data-next-btn]");
+
+const heroSliderItemsLength = heroSliderItems.length - 1;
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSliderPos = function () {
+  lastActiveSliderItem.classList.remove("active");
+  heroSliderItems[currentSlidePos].classList.add("active");
+  lastActiveSliderItem = heroSliderItems[currentSlidePos];
+};
+
+const slideNext = function () {
+  if (currentSlidePos >= heroSliderItemsLength) currentSlidePos = 0;
+  else currentSlidePos++;
+
+  updateSliderPos();
+};
+
+heroSliderNextBtn.addEventListener("click", slideNext);
+
+const slidePrev = function () {
+  if (currentSlidePos <= 0) currentSlidePos = heroSliderItemsLength;
+  else currentSlidePos--;
+
+  updateSliderPos();
+};
+
+heroSliderPrevBtn.addEventListener("click", slidePrev);
+
+// Auto Slide
+let autoSlideInterval;
+const autoSlide = function () {
+  autoSlideInterval = setInterval(() => {
+    slideNext();
+  }, 7000);
+};
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", () => {
+  clearInterval(autoSlideInterval);
+});
+
+addEventOnElements(
+  [heroSliderNextBtn, heroSliderPrevBtn],
+  "mouseout",
+  autoSlide
+);
+
+window.addEventListener("load", autoSlide);
